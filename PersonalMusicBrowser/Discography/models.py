29b504e932ec0445098e0bfd2f48052dfa55bdf1
NateDavis <nateasteroidd@gmail.com>
2021-01-18 12:40:56 -0500

    - added str to every model and removed all primary keys  - reset migrations after removing primary keys

--- PersonalMusicBrowser/Discography/models.py
+++ PersonalMusicBrowser/Discography/models.py
@@ -1,26 +1,36 @@
 from django.conf import settings
 from django.db import models
 
-import pathlib
-
 
 class Instrument(models.Model):
-    name = models.TextField(max_length="64", blank=False, primary_key=True)
+    name = models.TextField(max_length="64", blank=False)
+
+    def __str__(self):
+        return self.name
 
 
 class Band(models.Model):
     name = models.TextField(max_length=128, blank=False)
+
+    def __str__(self):
+        return self.name
 
 
 class Artist(models.Model):
     name = models.TextField(max_length=128, blank=False)
     bands = models.ManyToManyField(Band)
 
+    def __str__(self):
+        return self.name
+
 
 class Album(models.Model):
-    title = models.TextField(max_length=256, primary_key=True, blank=False)
+    title = models.TextField(max_length=256, blank=False)
     released = models.BooleanField(blank=False)
     url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
 
 
 def get_discography_path() -> str:
@@ -36,17 +46,26 @@ class Song(models.Model):
     lyrics = models.FilePathField(path=get_discography_path(), blank=True, allow_files=True, allow_folders=False,
                                   recursive=True)
 
+    def __str__(self):
+        return f"{self.title} -- {self.album}"
+
 
 class Cover(Song):
     notes = models.ImageField(blank=True)
     covered_instruments = models.ManyToManyField(Instrument)
     notes_completed = models.BooleanField()
 
+    def __str__(self):
+        return f"{super(self)} -- on {self.covered_instruments}"
+
 
 class Composition(Song):
     written_instruments = models.ManyToManyField(Instrument)
     beats_per_minute_upper = models.IntegerField()
     beats_per_minute_lower = models.IntegerField()
+
+    def __str__(self):
+        return f"{super(self)} -- arranged for {self.written_instruments}"
 
 
 class Recording(models.Model):
@@ -57,3 +76,6 @@ class Recording(models.Model):
                                 recursive=True)
     song = models.ForeignKey(Song, blank=False, on_delete=models.PROTECT)
     notes = models.ImageField(blank=True)
+
+    def __str__(self):
+        return f"{super(self)} -- in {self.type}"
